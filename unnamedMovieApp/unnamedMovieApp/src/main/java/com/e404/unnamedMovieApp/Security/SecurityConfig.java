42eaba202e5349c41a7f0dfb1f972440ce3a27a3
Kyle <kylenewton08@gmail.com>
2021-03-06 12:21:10 -0800

    Initial Testing Route

---
+++ unnamedMovieApp/unnamedMovieApp/src/main/java/com/e404/unnamedMovieApp/Security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.e404.unnamedMovieApp.Security;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+    }
+
+}
--- unnamedMovieApp/unnamedMovieApp/src/main/java/com/e404/unnamedMovieApp/UnnamedMovieAppApplication.java
+++ unnamedMovieApp/unnamedMovieApp/src/main/java/com/e404/unnamedMovieApp/UnnamedMovieAppApplication.java
@@ -1,6 +1,7 @@
 package com.e404.unnamedMovieApp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -9,5 +10,7 @@ public class UnnamedMovieAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UnnamedMovieAppApplication.class, args);
 	}
+
+
 
 }
