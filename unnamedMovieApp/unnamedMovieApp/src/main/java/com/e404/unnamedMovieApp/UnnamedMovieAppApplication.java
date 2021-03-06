537859686be747b8f85010d7b522bfe36bb4669d
Kyle <kylenewton08@gmail.com>
2021-03-06 11:26:46 -0800

    Added Initial Spring Boot Data

---
+++ unnamedMovieApp/unnamedMovieApp/src/main/java/com/e404/unnamedMovieApp/UnnamedMovieAppApplication.java
@@ -0,0 +1,13 @@
+package com.e404.unnamedMovieApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnnamedMovieAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnnamedMovieAppApplication.class, args);
+	}
+
+}
---
+++ unnamedMovieApp/unnamedMovieApp/src/test/java/com/e404/unnamedMovieApp/UnnamedMovieAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e404.unnamedMovieApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnnamedMovieAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
