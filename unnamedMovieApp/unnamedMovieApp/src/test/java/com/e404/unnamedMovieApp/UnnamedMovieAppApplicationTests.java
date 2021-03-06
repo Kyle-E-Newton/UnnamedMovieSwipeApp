0568a67a1f9dcc6c90c22f6d70b3adfd0ccfe383
Kyle <kylenewton08@gmail.com>
2021-03-06 13:52:19 -0800

    First Test Deploy for references

--- unnamedMovieApp/unnamedMovieApp/src/test/java/com/e404/unnamedMovieApp/UnnamedMovieAppApplicationTests.java
+++ unnamedMovieApp/unnamedMovieApp/src/test/java/com/e404/unnamedMovieApp/UnnamedMovieAppApplicationTests.java
@@ -1,5 +1,6 @@
 package com.e404.unnamedMovieApp;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class UnnamedMovieAppApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertTrue(true);
 	}
 
 }
